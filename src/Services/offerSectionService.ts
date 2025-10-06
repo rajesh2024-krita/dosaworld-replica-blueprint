6251cf1596fd654f5a7f8ea7eef4c1621a0e15ab
rajesh2024-krita <rajesh.kritatechnosolutions@gmail.com>
2025-10-06 17:17:04 +0530

    api changed

--- src/Services/offerSectionService.ts
+++ src/Services/offerSectionService.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 import { OfferSection, ApiResponse } from '../Types/OfferSection';
 
-const API_BASE_URL = 'http://localhost:3000/api';
+// const API_BASE_URL = 'http://localhost:3000/api';
+const API_BASE_URL = 'https://dosaworld-backend-xypt.onrender.com/api';
 
 const api = axios.create({
   baseURL: API_BASE_URL,
